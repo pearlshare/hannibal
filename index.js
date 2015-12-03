8acdb766c5b426d5ae055f4fadcd19cd9d746a5b
orangemug <info@orangemug.co.uk>
2015-12-03 19:55:15 +0000

    Added optional basic type transforms

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require("./lib/index");
+var assign   = require("lodash.assign");
+var Hannibal = require("./lib/index");
+var basic    = require("./lib/transforms/basic");
+
+function HannibalWithTransforms(opts) {
+  opts = opts || {};
+  opts.transforms = assign({}, opts.transforms, basic);
+  return Hannibal.call(this, opts);
+}
+
+HannibalWithTransforms.prototype = Object.create(Hannibal.prototype);
+
+module.exports = HannibalWithTransforms;
