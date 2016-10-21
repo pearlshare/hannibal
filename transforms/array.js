a2aac6b5ef3366ff7b9b0e2e2975192f4a2588a2
Oliver Brooks <oli@pearlshare.com>
2016-10-21 15:27:27 +0100

    add circleci configuration

--- transforms/array.js
+++ transforms/array.js
@@ -1,23 +1,22 @@
 var lodash = {
   uniq:    require("lodash.uniq"),
-  isArray: require("lodash.isarray"),
   compact: require("lodash.compact")
 };
 
 
 module.exports = {
   uniq: function (value) {
-    if (lodash.isArray(value)) {
+    if (Array.isArray(value)) {
       return lodash.uniq(value);
     } else {
       return value;
     }
   },
   compact: function (value) {
-    if (lodash.isArray(value)) {
+    if (Array.isArray(value)) {
       return lodash.compact(value);
     } else {
       return value;
     }
   }
-}
+};
